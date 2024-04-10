080b7c0123c2c0533947232ea72a4c0887842ac8
Akitovi <147024708+Akitovi@users.noreply.github.com>
2024-04-10 14:21:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// array to store task
+let tasks=[];
+const renderTasks=()=>{
+    let taskList= document.getElementById("taskList");
+    taskList.innerHTML='';
+
+    tasks.forEach((task,index)=> {
+      const li=document.createElement('li');  
+      li.textContent=task.name;
+      const btn=document.createElement("button");
+      btn.innerHTML='<div>delete</div>';
+      btn.className='delete-button';
+      btn.onclick=()=>deleteTask(index);
+
+      li.appendChild(btn);
+      taskList.appendChild(li);
+      
+    });
+}
+
+const addTask=()=>{
+    const name=document.getElementById('taskInput');
+    if(name!==''){
+        const task={
+            name:name.value,
+            date:new Date()
+        }
+        tasks.push(task);
+        renderTasks();
+     };
+}
+
+const deleteTask =(index)=>{
+    tasks.splice(index,1);
+    renderTasks();
+}
+const countTask=()=>{
+    alert("total number of tasks is: " + tasks.length)
+
+}
+const clearAllTask=()=>{
+    tasks=[];
+    renderTasks();
+}
